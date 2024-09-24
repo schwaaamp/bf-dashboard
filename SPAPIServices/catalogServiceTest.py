e25a26fecf1db7957eb627daed2ac740d6432025
Erin Osterfeld <erin.osterfeld@gmail.com>
2024-09-24 14:59:09 -0400

    Deleting dupe folder

--- SPAPIServices/catalogServiceTest.py
+++
@@ -1,7 +0,0 @@
-from SPAPIServices.catalogService import CatalogService
-
-service = CatalogService()
-df = service.getSearchResults()
-print(df)
-
-print(df.loc[df.isin(['Buckeye Farms']).any(axis=1)].index.tolist())
