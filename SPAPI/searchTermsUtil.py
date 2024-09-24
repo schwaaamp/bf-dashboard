c01756f2c6ce18655c74728842fad9a572a5de5c
Erin Osterfeld <erin.osterfeld@gmail.com>
2024-09-24 16:27:11 -0400

    remove searchTermsUtil

--- SPAPI/searchTermsUtil.py
+++
@@ -1,6 +0,0 @@
-searchTerms = dict(
-    HFDCB='Harbor Freight Dust Collector Bags',
-    SFDCB='Shop fox Dust Collector Bags',
-    JDCB='Jet Dust Collector Bags',
-    GDCB='Grizzly Dust Collector Bags'
-)
